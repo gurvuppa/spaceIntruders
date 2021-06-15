8eddfeaa46d51013d8d2c2c71d0a00068fdbdb5b
Gurvir Uppal <gurviruppal@icloud.com>
2021-06-15 14:16:05 -0400

    Add project files.

---
+++ spaceIntruders/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace spaceIntruders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subTitleLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.livesLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Lime;
+            this.titleLabel.Location = new System.Drawing.Point(67, 235);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(450, 27);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "titleLabel";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // subTitleLabel
+            // 
+            this.subTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subTitleLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTitleLabel.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.subTitleLabel.Location = new System.Drawing.Point(68, 262);
+            this.subTitleLabel.Name = "subTitleLabel";
+            this.subTitleLabel.Size = new System.Drawing.Size(450, 27);
+            this.subTitleLabel.TabIndex = 4;
+            this.subTitleLabel.Text = "subTitleLabel";
+            this.subTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.Lime;
+            this.scoreLabel.Location = new System.Drawing.Point(2, 0);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(100, 23);
+            this.scoreLabel.TabIndex = 5;
+            this.scoreLabel.Text = "scoreLabel";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // livesLabel
+            // 
+            this.livesLabel.BackColor = System.Drawing.Color.Transparent;
+            this.livesLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.livesLabel.ForeColor = System.Drawing.Color.Lime;
+            this.livesLabel.Location = new System.Drawing.Point(457, 0);
+            this.livesLabel.Name = "livesLabel";
+            this.livesLabel.Size = new System.Drawing.Size(100, 23);
+            this.livesLabel.TabIndex = 6;
+            this.livesLabel.Text = "livesLabel";
+            this.livesLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.livesLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.subTitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subTitleLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label livesLabel;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ spaceIntruders/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace spaceIntruders
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player = new Rectangle(280, 540, 40, 10);
+
+
+        int moveTimer = 0;
+
+        int alienSpeed = 1;
+
+        int playerSpeed = 10;
+
+        List<Rectangle> alien = new List<Rectangle>();
+        List<int> alienSpeeds = new List<int>();
+
+        int alienSize = 25;
+
+        int score = 0;
+
+        bool aDown = false;
+        bool dDown = false;
+
+        string gameState = "waiting";
+
+        SolidBrush greenBrush = new SolidBrush(Color.LimeGreen);
+
+        Image blue = Properties.Resources.alien2;
+
+        Random randGen = new Random();
+        int randValue = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GameInitialize()
+        {
+
+            titleLabel.Text = "";
+            subTitleLabel.Text = "";
+
+            gameTimer.Enabled = true;
+            gameState = "running";
+            score = 0;
+            alien.Clear();
+            alienSpeeds.Clear();
+
+            player.X = 280;
+            player.Y = 540;
+
+            //spawn alien
+            spawnAlien();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Space:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        GameInitialize();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+            //move player
+            movePlayer();
+
+            moveAlien();
+
+            moveTimer++;
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameState == "waiting")
+            {
+                livesLabel.Text = "";
+                scoreLabel.Text = "";
+                titleLabel.Text = "SPACE INTRUDERS";
+                subTitleLabel.Text = "Press Space Bar to Start or Escape to Exit";
+            }
+            else if (gameState == "running")
+            {
+                e.Graphics.FillRectangle(greenBrush, player);
+
+                for (int i = 0; i < alien.Count(); i++)
+                {
+                    //e.Graphics.FillRectangle(greenBrush, alien[i]);
+                    e.Graphics.DrawImage(blue, alien[i]);
+                }
+            }
+        }
+
+        //Move player
+        public void movePlayer()
+        {
+            if (aDown == true && player.X > 0)
+            {
+                player.X -= playerSpeed;
+            }
+
+            if (dDown == true && player.X < this.Width - player.Width)
+            {
+                player.X += playerSpeed;
+            }
+        }
+
+        //spawn aliens
+        public void spawnAlien()
+        {
+            int rows = 5;           // number of rows
+            int numbAliens = 11;    //number of aliens in each row
+            int gap = 20;           // gap in between each alien
+            int x = 20;             //starting x
+            int y = 50;             //starting y
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int n = 0; n < numbAliens; n++)
+                {
+                    x += alienSize + gap;
+                    alien.Add(new Rectangle(x, y, alienSize, alienSize));
+                }
+
+                y += alienSize + gap;
+                x = 20;
+            }
+        }
+
+        //move aliens
+        public void moveAlien()
+        {
+            for (int i = 0; i < alien.Count(); i++)
+            {
+                int x = alien[i].X + alienSpeed;
+                alien[i] = new Rectangle(x, alien[i].Y, alienSize, alienSize);
+            }
+        }
+
+
+
+
+    }
+}
